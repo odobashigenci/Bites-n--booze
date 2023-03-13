72345e977ec6379aad35c8ba8589d5055caec7a0
Daelen Wall <daelenwall1@gmail.com>
2023-03-13 11:09:40 -0400

    sidebar js

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,15 @@
 var hungryBtn = document.getElementById("hungry")
 var thirstyBtn = document.getElementById("thirsty")
+//quick dish btns
+var sushiBtn = document.getElementById("sushi")
+var fettuccineBtn = document.getElementById("fettuccine")
+var chivitoBtn = document.getElementById("chivito")
+var stewBtn = document.getElementById("stew")
+//quick drink btns
+var daiquiriBtn = document.getElementById("daiquiri")
+var margaritaBtn = document.getElementById("margarita")
+var mojitoBtn = document.getElementById("mojito")
+var negroniBtn = document.getElementById("negroni")
 
 //setting up the buttons to direct to next page to click
 hungryBtn.addEventListener("click", () => {
@@ -10,6 +20,39 @@ thirstyBtn.addEventListener("click", () => {
     window.location.assign("drink.html");
 })
 
+// quick dishes sidebar event listeners
+sushiBtn.addEventListener("click", () => {
+    window.location.assign("https://www.themealdb.com/meal/53065-Sushi-Recipe")
+})
+
+fettuccineBtn.addEventListener("click", () => {
+  window.location.assign("https://www.themealdb.com/meal/53064-Fettuccine-Alfredo-Recipe")
+})
+
+chivitoBtn.addEventListener("click", () => {
+  window.location.assign("https://www.themealdb.com/meal/53063-Chivito-uruguayo-Recipe")
+})
+
+stewBtn.addEventListener("click", () => {
+  window.location.assign("https://www.themealdb.com/meal/53058-Croatian-Bean-Stew-Recipe")
+})
+
+// quick drinks sidebar event listeners
+daiquiriBtn.addEventListener("click", () => {
+  window.location.assign("https://www.thecocktaildb.com/drink/11006-Daiquiri-Cocktail")
+})
+
+margaritaBtn.addEventListener("click", () => {
+window.location.assign("https://www.thecocktaildb.com/drink/11007-Margarita-Cocktail")
+})
+
+mojitoBtn.addEventListener("click", () => {
+window.location.assign("https://www.thecocktaildb.com/drink/11000-Mojito-Cocktail")
+})
+
+negroniBtn.addEventListener("click", () => {
+window.location.assign("https://www.thecocktaildb.com/drink/11003-Negroni-Cocktail")
+})
 
 // setting up the navbar drop down menu (burger)
 $(document).ready(function() {
